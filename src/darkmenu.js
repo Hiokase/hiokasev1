a6a1a8b46045aef4297403aa66be02f0ce38d1d7
Hiokase <72773201+Hiokase@users.noreply.github.com>
2021-04-04 13:44:04 -0400

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,35 @@
+const darkmenu = (prefix) => {
+	return `
+
+      COMANDOS:
+
+  *Comandos do bot:*
+
+ *${prefix}loli*
+ *${prefix}dono*
+ *${prefix}boanoite*
+ *${prefix}bomdia*
+ *${prefix}boatarde*
+ *${prefix}mia*
+ *${prefix}mia1*
+ *${prefix}mia2*
+ *${prefix}belle*
+ *${prefix}belle1*
+ *${prefix}belle2*
+ *${prefix}belle3*
+ *${prefix}ayeko*
+
+╔════════════════════
+  WA.me/5569999662600
+╚════════════════════`
+}
+
+exports.darkmenu = darkmenu
+
+
+
+
+
+
+
+
